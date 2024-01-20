209b3e13140f313fc34e0be952488fadf00833bd
Krystian Szpiczakowski <szpiczakowski.krystian@gmail.com>
2024-01-20 23:50:15 +0100

    Implement Part 4.4 - Unit tests

--- part4/bloglist/test/list_helper.test.js
+++ part4/bloglist/test/list_helper.test.js
@@ -1,6 +1,40 @@
-const listHelper = require('../util/list_helper')
+const listHelper = require("../util/list_helper");
 
 test("Dummy returns one", () => {
+  const blogs = [];
+  expect(listHelper.dummy(blogs)).toBe(1);
+});
+
+describe("Total likes", () => {
+  test("When list is empty return zero", () => {
     const blogs = [];
-    expect(listHelper.dummy(blogs)).toBe(1);
-})
+    expect(listHelper.totalLikes(blogs)).toBe(0);
+  });
+
+  test("When list has only one blog, equals the likes of that", () => {
+    const blogs = [
+        {
+            author: "Someone",
+            title: "How to do nothing",
+            likes: 3
+        }
+    ];
+    expect(listHelper.totalLikes(blogs)).toBe(3);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const blogs = [
+        {
+            author: "Someone",
+            title: "How to do nothing",
+            likes: 3
+        },
+        {
+            author: "Someone else",
+            title: "How to do something",
+            likes: 5
+        }
+    ];
+    expect(listHelper.totalLikes(blogs)).toBe(8);
+  });
+});
--- part4/bloglist/util/list_helper.js
+++ part4/bloglist/util/list_helper.js
@@ -2,4 +2,16 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  if (!blogs) {
+    return 0;
+  }
+
+  const reducer = (sum, item) => sum + item;
+  const sumOfLikes = blogs
+    .map(item => item.likes)
+    .reduce(reducer, 0);
+  return sumOfLikes;
+}
+
+module.exports = { dummy, totalLikes };
